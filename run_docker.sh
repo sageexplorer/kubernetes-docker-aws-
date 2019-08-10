0c7f9aa382a6bfcebd23915e951fe19b7044b068
Sage <srimal@campusexplorer.com>
2019-08-10 16:49:58 -0700

    stop containers before running another

--- run_docker.sh
+++ run_docker.sh
@@ -4,4 +4,10 @@ docker build --tag=simple-ngnix .
 
 docker image ls
 
+# Remove container before running
+docker stop $(docker ps -a -q)
+
 docker run  -d -p 80:80 simple-ngnix
+
+
+#sudo lsof -i -n -P | grep TCP
