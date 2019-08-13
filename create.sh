091a36bc045e828cb9f342ab0443694c58f7d80e
Sage Rimal <srimal@campusexplorer.com>
2019-08-13 16:07:44 -0700

    Delete create.sh

--- create.sh
+++
@@ -1,7 +0,0 @@
-aws cloudformation create-stack \
---profile {} \
---stack-name $1 \
---template-body file://$2 \
---parameters file://$3 \
---capabilities CAPABILITY_NAMED_IAM \
---region=us-west-2
